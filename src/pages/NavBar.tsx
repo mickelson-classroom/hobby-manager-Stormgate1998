3fabbdbd3462233da96fcf637093e2edef1cb6a1
Joshua Barlow <jalapenojosh@gmail.com>
2023-10-01 00:04:10 -0600

    Added picture capability

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -15,23 +15,26 @@ const Navbar: React.FC<NavbarProps> = () => {
       <div className="container">
         <div className="nav-item">
           <Link to="/" className="nav-link">
-            
-            <div  className=' icon purple-icon'><GhostIcon/></div>
+            <div className="icon purple-icon">
+              <GhostIcon />
+            </div>
             <h3 className="text-primary">Home</h3>
           </Link>
         </div>
         <div className="nav-item">
           <Link to="/error" className="nav-link">
-            
-            <div className='icon green-icon'><MagicIcon /></div>
+            <div className="icon green-icon">
+              <MagicIcon />
+            </div>
             <h3 className="text-primary">Error Testing</h3>
           </Link>
         </div>
         <div className="nav-item">
-          <Link to="/toast" className="nav-link ">
-            <div className='icon red-icon'><CommentIcon /></div>
-            
-            <h3 className="text-primary ">The Toaster Oven</h3>
+          <Link to="/toast" className="nav-link">
+            <div className="icon red-icon">
+              <CommentIcon />
+            </div>
+            <h3 className="text-primary">The Toaster Oven</h3>
           </Link>
         </div>
       </div>
