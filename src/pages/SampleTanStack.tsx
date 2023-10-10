bebc868071412dc64fd5db07a84ce9b4f7374627
Joshua Barlow <jalapenojosh@gmail.com>
2023-10-09 21:19:39 -0600

    removed dispatch bug, started on tanstack

--- src/pages/SampleTanStack.tsx
+++ src/pages/SampleTanStack.tsx
@@ -6,15 +6,34 @@ import {
 import React, { ChangeEvent, useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from './NavBar';
+import { Spinner } from '../services/Spinner';
 
 const queryClient = new QueryClient();
 
 export const TanStackExampleCode = () => {
     const [sampleThing, setSampleThing] = useState(false);
+    const { isLoading, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return(<Spinner/>)
+
+  if (error) return(<div>'An error has occurred: ' + error.message</div>)
+
     return (
         <>
             <Navbar />
-            <div>hi</div>
+            <div>
+                <h1>{data.name}</h1>
+                    <p>{data.description}</p>
+                    <strong>👀 {data.subscribers_count}</strong>{' '}
+                    <strong>✨ {data.stargazers_count}</strong>{' '}
+                    <strong>🍴 {data.forks_count}</strong>
+            </div>
         </>
     );
 };
