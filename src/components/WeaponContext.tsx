b6accd4c53da7e7b0813d2461453473e6a052753
Joshua Barlow <jalapenojosh@gmail.com>
2023-10-16 10:18:48 -0600

    fixed some stuff for linter

--- src/components/WeaponContext.tsx
+++ src/components/WeaponContext.tsx
@@ -1,128 +1,123 @@
-// import * as React from 'react';
-// import { Weapon} from '../models/weapons';
+import * as React from 'react';
+import { Weapon} from '../models/weapons';
 
-// import { getWeapons, weapons } from '../services/weaponService';
-// import { useEffect, useState } from 'react';
+import { getWeapons, weapons } from '../services/weaponService';
+import { useEffect, useState } from 'react';
 
-// //export const WeaponContext = React.createContext<WeaponContextType | null>(null);
+//export const WeaponContext = React.createContext<WeaponContextType | null>(null);
 
-// interface WeaponContext {
-//   weapons: Weapon[];
-//   saveWeapons: (weapon: Weapon) => void;
-//   updateWeapons: (weapon: Weapon) => void;
-//   deleteWeapons: (id: string) => void;
-// }
+interface WeaponContext {
+  weapons: Weapon[];
+  saveWeapons: (weapon: Weapon) => void;
+  updateWeapons: () => void;
+  deleteWeapons: () => void;
+}
 
-// const defaultWeapons: WeaponContext = {
-//   weapons: [
-//     {
-//       id: "1",
-//       name: "Longsword",
-//       material: "Steel",
-//       typeofDamage: "Slashing",
-//       range: "Melee",
-//       imgUrl: "",
-//     },
-//     {
-//       id: "2",
-//       name: "Bow",
-//       material: "Wood and String",
-//       typeofDamage: "Piercing",
-//       range: "Ranged",
-//       imgUrl: "",
-//     },
-//     {
-//       id: "3",
-//       name: "Warhammer",
-//       material: "Iron",
-//       typeofDamage: "Blunt",
-//       range: "Melee",
-//       imgUrl: "",
-//     }
-//   ],
-//   saveWeapons: (weapon: Weapon) => {
-//     const maxId = weapons.reduce((max, weapon) => {
-//     const weaponId = parseInt(weapon.id);
-//      return weaponId > max ? weaponId : max;
-//     }, -1);
-//     const newWeapon: Weapon = {
-//       id: (maxId + 1).toString(),
-//       name: weapon.name,
-//       material: weapon.material,
-//       typeofDamage: weapon.typeofDamage,
-//       range: weapon.range,
-//       imgUrl: weapon.imgUrl
-//     }
-//     defaultWeapons.weapons = [...defaultWeapons.weapons, newWeapon]
-//   },
-//   deleteWeapons(id: string) {
-//   },
-//   updateWeapons(weapon) {
-//   }
-// };
+const defaultWeapons: WeaponContext = {
+  weapons: [
+    {
+      id: "1",
+      name: "Longsword",
+      material: "Steel",
+      typeofDamage: "Slashing",
+      range: "Melee",
+      imgUrl: "",
+    },
+    {
+      id: "2",
+      name: "Bow",
+      material: "Wood and String",
+      typeofDamage: "Piercing",
+      range: "Ranged",
+      imgUrl: "",
+    },
+    {
+      id: "3",
+      name: "Warhammer",
+      material: "Iron",
+      typeofDamage: "Blunt",
+      range: "Melee",
+      imgUrl: "",
+    }
+  ],
+  saveWeapons: (weapon: Weapon) => {
+    const maxId = weapons.reduce((max, weapon) => {
+    const weaponId = parseInt(weapon.id);
+     return weaponId > max ? weaponId : max;
+    }, -1);
+    const newWeapon: Weapon = {
+      id: (maxId + 1).toString(),
+      name: weapon.name,
+      material: weapon.material,
+      typeofDamage: weapon.typeofDamage,
+      range: weapon.range,
+      imgUrl: weapon.imgUrl
+    }
+    defaultWeapons.weapons = [...defaultWeapons.weapons, newWeapon]
+  },
+  deleteWeapons() {
+  },
+  updateWeapons() {
+  }
+};
 
-// const weaponStorageKey = "storedWeapons";
-// const storeWeaponsInLocalStorage = (weapons: Weapon[]) => {
-//   localStorage.setItem(weaponStorageKey, JSON.stringify(weapons))
-// }
+const weaponStorageKey = "storedWeapons";
+const storeWeaponsInLocalStorage = (weapons: Weapon[]) => {
+  localStorage.setItem(weaponStorageKey, JSON.stringify(weapons))
+}
 
-// const getWeaponsInLocalStorage = (): Weapon[] =>{
-//   const returnedWeapons = localStorage.getItem(weaponStorageKey);
-//   return JSON.parse(returnedWeapons ?? "[]")
-// }
-// export const WeaponContext = React.createContext<WeaponContext>(defaultWeapons);
+const getWeaponsInLocalStorage = (): Weapon[] =>{
+  const returnedWeapons = localStorage.getItem(weaponStorageKey);
+  return JSON.parse(returnedWeapons ?? "[]")
+}
 
-
-// export const WeaponProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
-//   const [weapons, setWeapons] = useState<Weapon[]>(getWeaponsInLocalStorage())
-//   console.log(weapons)
-//   useEffect (() =>{
-//     const storedWeapons = getWeaponsInLocalStorage();
-//     if (storedWeapons.length > 0){
-//       setWeapons(storedWeapons)
-//     } else{
-//       getWeapons().then((newWeapons) => setWeapons(newWeapons))
-//     }
-//   }, [])
-
-//   useEffect(() => {
-//     console.log("Actually updating the weapon list")
-//     storeWeaponsInLocalStorage(weapons)
-//   }, [weapons])
+export const myWeaponContext = React.createContext<WeaponContext>(defaultWeapons);
 
 
-//   const saveWeapons = (weapon: Weapon) => {
-//     const newWeapon: Weapon = {
-//       id: (Date.now).toString(),
-//       name: weapon.name,
-//       material: weapon.material,
-//       typeofDamage: weapon.typeofDamage,
-//       range: weapon.range,
-//       imgUrl: weapon.imgUrl
-//     }
-//     setWeapons((newWeapons) => [...newWeapons, newWeapon])
-//   }
+export const WeaponProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
+  const [weapons, setWeapons] = useState<Weapon[]>(getWeaponsInLocalStorage())
+  console.log(weapons)
+  useEffect (() =>{
+    const storedWeapons = getWeaponsInLocalStorage();
+    if (storedWeapons.length > 0){
+      setWeapons(storedWeapons)
+    } else{
+      getWeapons().then((newWeapons) => setWeapons(newWeapons))
+    }
+  }, [])
 
-//   const updateWeapons = (weapon: Weapon) => {
-//     setWeapons((oldWeapons) => [
-//       weapon, 
-//       ...oldWeapons.filter( (w) => w.id !== weapon.id)
-//     ])
-//   }
+  useEffect(() => {
+    console.log("Actually updating the weapon list")
+    storeWeaponsInLocalStorage(weapons)
+  }, [weapons])
 
-//   const deleteWeapons = (id: string) => {
-//     const newWeapons = weapons.filter(w => w.id !== id)
-//     setWeapons(newWeapons)
-//   }
 
-//   return (
-//     <WeaponContext.Provider
-//         value={{
-//           weapons,
-//           saveWeapons,
-//           updateWeapons,
-//           deleteWeapons
-//         }}
-//         >{children}</WeaponContext.Provider>
-//   )
-// }
+  const saveWeapons = (weapon: Weapon) => {
+    const newWeapon: Weapon = {
+      id: (Date.now).toString(),
+      name: weapon.name,
+      material: weapon.material,
+      typeofDamage: weapon.typeofDamage,
+      range: weapon.range,
+      imgUrl: weapon.imgUrl
+    }
+    setWeapons((newWeapons) => [...newWeapons, newWeapon])
+  }
+
+  const updateWeapons = () => {
+  }
+
+  const deleteWeapons = () => {
+  }
+
+  return (
+    <myWeaponContext.Provider
+        value={{
+          weapons,
+          saveWeapons,
+          updateWeapons,
+          deleteWeapons
+        }}
+        >{children}</myWeaponContext.Provider>
+  )
+}
---
+++ src/features/weapon-slice.ts
@@ -0,0 +1,153 @@
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import { Weapon } from '../models/weapons';
+import { hammerName, swordName, wandName, staffName, bowName } from './filenames';
+import { weaponAPIService } from '../services/weaponApiCalls';
+
+interface WeaponState {
+    weapons: Weapon[],
+    loading: boolean
+}
+
+export const initialState: WeaponState={ 
+    loading: false,
+    weapons: [
+    {
+    id: "1",
+    name: "Longsword",
+    material: "Steel",
+    typeofDamage: "Slashing",
+    range: "Melee",
+    imgUrl: swordName,
+  },
+  {
+    id: "2",
+    name: "Bow",
+    material: "Wood and String",
+    typeofDamage: "Piercing",
+    range: "Ranged",
+    imgUrl: bowName,
+  },
+  {
+    id: "3",
+    name: "Warhammer",
+    material: "Iron",
+    typeofDamage: "Blunt",
+    range: "Melee",
+    imgUrl: hammerName,
+  },
+  {
+    id: "4",
+    name: "Bonky Stick",
+    material: "Wood",
+    typeofDamage: "Blunt",
+    range: "Melee",
+    imgUrl: staffName,
+  },
+  {
+    id: "5",
+    name: "Magic Wand",
+    material: "Wood",
+    typeofDamage: "Magic",
+    range: "Mid-Ranged",
+    imgUrl: wandName,
+  }
+]
+
+}
+
+// Create async thunk for saving weapons
+export const addWeaponsThunk = createAsyncThunk(
+  'weapons/saveWeapons',
+   async (item: Weapon) => {
+    console.log("beginning")
+    await weaponAPIService.addWeapon(item)
+    return await weaponAPIService.getWeapons()
+  }
+);
+
+// Create async thunk for updating weapons
+export const updateWeaponsThunk = createAsyncThunk(
+  'weapons/updateWeapons',
+  async (item: Weapon): Promise<Weapon[]>  => {
+    await weaponAPIService.updateWeapon(item)
+    return await weaponAPIService.getWeapons()
+  }
+);
+
+// Create async thunk for deleting weapons
+export const deleteWeaponsThunk = createAsyncThunk(
+  'weapons/deleteWeapons',
+  async (item: Weapon): Promise<Weapon[]> => {
+    await weaponAPIService.deleteWeapon(item);
+
+    const updatedWeapons = await weaponAPIService.getWeapons();
+    return updatedWeapons;
+  }
+);
+
+export const getWeaponsThunk = createAsyncThunk(
+  'weapons/getWeapons',
+   async (): Promise<Weapon[]> => {
+    return await weaponAPIService.getWeapons()
+  }
+)
+
+const weaponSlice = createSlice({
+    name: "weapons",
+    initialState,
+    reducers: {
+        setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload
+    },
+    },
+     extraReducers: (builder) => {
+    builder.addCase(addWeaponsThunk.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(addWeaponsThunk.fulfilled, (state, action) => {
+      state.loading = false
+      state.weapons = action.payload
+    })
+    builder.addCase(addWeaponsThunk.rejected, (state) => {
+      state.loading = false
+      //state.error = action.error.message
+    })
+    builder.addCase(updateWeaponsThunk.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(updateWeaponsThunk.fulfilled, (state, action) => {
+      state.loading = false
+      state.weapons = action.payload
+    })
+    builder.addCase(updateWeaponsThunk.rejected, (state) => {
+      state.loading = false
+      // state.error = action.error.message
+    })
+    builder.addCase(deleteWeaponsThunk.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(deleteWeaponsThunk.fulfilled, (state, action) => {
+      state.loading = false
+      state.weapons = action.payload
+    })
+    builder.addCase(deleteWeaponsThunk.rejected, (state) => {
+      state.loading = false
+      // state.error = action.error.message
+    })
+    builder.addCase(getWeaponsThunk.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(getWeaponsThunk.fulfilled, (state, action) => {
+      state.loading = false
+      state.weapons = action.payload
+    })
+    builder.addCase(getWeaponsThunk.rejected, (state) => {
+      state.loading = false
+      // state.error = action.error.message
+    })
+  }
+
+});
+
+export const {setLoading} = weaponSlice.actions;
+export default weaponSlice.reducer;
